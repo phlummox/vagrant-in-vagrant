2df8c2c4b7938e9f101c53315df69b650611c1da
phlummox <phlummox@localhost>
2021-03-25 15:17:03 +0000

    try no key

--- test-vm.sh
+++ test-vm.sh
@@ -18,6 +18,15 @@ vagrant init vinv
 
 vagrant up --provider libvirt
 
+cat > /tmp/Vagrantfile << 'END'
+Vagrant.configure("2") do |config|
+  config.vm.box = "generic/alpine312"
+  config.ssh.password = "vagrant"
+  config.ssh.username = "vagrant"
+  config.ssh.insert_key = false
+end
+END
+
 cat > test-inner-vm.sh <<'END'
 set -ex
 
@@ -37,13 +46,25 @@ sudo iptables -F
 sudo iptables --list
 
 vagrant box add --provider libvirt generic/alpine312
-vagrant init generic/alpine312
-vagrant up --debug --provider libvirt
-the_pwd=`vagrant ssh -- pwd`
-[ "$the_pwd" = "/home/vagrant" ]
+#vagrant init generic/alpine312
+cp /tmp/Vagrantfile .
+
+vagrant ssh-config | sed 's/PasswordAuthentication no/PasswordAuthentication yes/' > ssh-config
+
+vagrant up --no-provision --debug --provider libvirt
+
+sudo apt install sshpass
+
+sshpass -pvagrant ssh -F ./ssh-config default pwd > pwd_result
+
+pwd_result=`cat pwd_result`
+
+[ "$pwd_result" = "/home/vagrant" ]
 END
 
 chmod a+rx test-inner-vm.sh
+
+vagrant upload /tmp/Vagrantfile /tmp/Vagrantfile
 
 vagrant upload ./test-inner-vm.sh /tmp/test-inner-vm.sh
 
